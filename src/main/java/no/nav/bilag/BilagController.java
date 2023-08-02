c1dd18943653ef9ce2e9f5bf7a21dc3b4d1bbe0d
Anders Opskar Voldsund <anders.voldsund@nav.no>
2023-08-02 15:47:44 +0200

    Legg til logging

--- src/main/java/no/nav/bilag/BilagController.java
+++ src/main/java/no/nav/bilag/BilagController.java
@@ -2,10 +2,12 @@ package no.nav.bilag;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController("/rest")
+@RestController
+@RequestMapping("/rest")
 public class BilagController {
 
 	@GetMapping("/hentDokument")
