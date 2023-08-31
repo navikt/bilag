69b2a513fd1f4857c22a35ed645dd5b8f0f4305c
Anders Opskar Voldsund <anders.voldsund@nav.no>
2023-08-31 11:39:23 +0200

    MMA-6913 Test exceptionHandler med retur av http-side

--- src/main/java/no/nav/bilag/BilagController.java
+++ src/main/java/no/nav/bilag/BilagController.java
@@ -2,22 +2,16 @@ package no.nav.bilag;
 
 import com.nimbusds.oauth2.sdk.token.AccessToken;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.constraints.Positive;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.bilag.auth.OauthService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static java.lang.String.format;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.APPLICATION_PDF_VALUE;
 import static org.springframework.http.MediaType.valueOf;
 
@@ -27,7 +21,6 @@ import static org.springframework.http.MediaType.valueOf;
 @RequestMapping
 public class BilagController {
 
-	private static final String BILAG_FUNKSJONELL_FEILMELDING = "bilag feilet funksjonelt med feilmelding: {}";
 	private final BrevserverConsumer brevserverConsumer;
 	private final OauthService oauthService;
 
@@ -49,18 +42,4 @@ public class BilagController {
 		return ResponseEntity.ok().contentType(valueOf(APPLICATION_PDF_VALUE)).body(dokument);
 	}
 
-	@ExceptionHandler({
-			ConstraintViolationException.class,
-	})
-	public ResponseEntity<Object> inputValidationExceptionHandler(Exception e) {
-		log.warn(BILAG_FUNKSJONELL_FEILMELDING, e.getMessage(), e);
-
-		return getResponseEntity(BAD_REQUEST, e.getMessage());
-	}
-
-	private static ResponseEntity<Object> getResponseEntity(HttpStatus status, String message) {
-		return ResponseEntity.status(status)
-				.contentType(APPLICATION_JSON)
-				.body(format("\"%s\"", message));
-	}
 }
---
+++ src/main/java/no/nav/bilag/BilagExceptionHandler.java
@@ -0,0 +1,51 @@
+package no.nav.bilag;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.MediaType.TEXT_HTML;
+
+@Slf4j
+@ControllerAdvice
+public class BilagExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		String feilmelding = ex.getFieldErrors().stream()
+				.map(it -> format("%s, mottatt %s=%s", it.getDefaultMessage(), it.getField(), it.getRejectedValue()))
+				.collect(Collectors.joining(". "));
+
+		log.warn("Validering av request feilet med feil={}", feilmelding, ex);
+
+		return ResponseEntity.status(BAD_REQUEST)
+				.contentType(TEXT_HTML)
+				.body(badRequestHttpPage());
+	}
+
+	private static String badRequestHttpPage() {
+		return """
+				<!DOCTYPE html>
+				<html lang="no">
+				<head>
+					<meta charset="UTF-8">
+				    <title>DokId er ugyldig</title>
+				</head>
+				<body>
+				    <h1>DokId er ikke en gyldig dokumentId</h1>
+				    <p>Dokid må bestå av et positivt tall.</p>
+				    <p>Prøv igjen med en gyldig dokumentId, eller kontakt Team Dokumentløsninger gjennom brukerstøtte eller på Slack-kanalen #team_dokumentløsninger.</p>
+				</body>
+				</html>
+				""";
+	}
+}
