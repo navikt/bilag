67047002dc847ce56cccbbdb81dc49b653cc2a30
Anders Opskar Voldsund <anders.voldsund@nav.no>
2023-09-11 10:00:39 +0200

    MMA-6913 Fjern jsonIgnoreProperties som uansett er default i spring

--- src/main/java/no/nav/bilag/auth/TokenResponse.java
+++ src/main/java/no/nav/bilag/auth/TokenResponse.java
@@ -1,9 +1,7 @@
 package no.nav.bilag.auth;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public record TokenResponse(
 		@JsonProperty(value = "access_token", required = true)
 		String accessToken
